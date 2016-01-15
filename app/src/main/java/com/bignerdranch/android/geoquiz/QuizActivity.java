f61eb5f21fa4b65ef16894c60da79f5b208943bd
Richard Hoisington <Richard Hoisington>
2016-01-14 21:18:25 -0500

    Chapter 3 alterations

--- app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/bignerdranch/android/geoquiz/QuizActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.geoquiz;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -9,6 +10,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
+
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
+    private static final String KEY_SCORE = "score";
 
     private Button mTrueButton;
     private Button mFalseButton;
@@ -78,6 +83,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) { //Why does everything go here?
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onStart() called");
         setContentView(R.layout.activity_quiz);
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
@@ -122,7 +128,49 @@ public class QuizActivity extends AppCompatActivity {
 
         mScoreTextView = (TextView) findViewById(R.id.score_text_view);
 
+        if(savedInstanceState != null){
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
         updateQuestion();
         updateScore();
     }
+
+        @Override
+        public void onSaveInstanceState (Bundle savedInstanceState){
+            super.onSaveInstanceState(savedInstanceState); // The redundancy is interesting
+            Log.i(TAG, "onSaveInstanceState");
+            savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+            savedInstanceState.putInt(KEY_SCORE,score);
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            Log.d(TAG, "onStart() called");
+        }
+
+        @Override
+        public void onPause() {
+            super.onPause();
+            Log.d(TAG, "onPause() called");
+        }
+
+        @Override
+        public void onResume() {
+            super.onResume();
+            Log.d(TAG, "onResume() called");
+        }
+
+        @Override
+        public void onStop() {
+            super.onStop();
+            Log.d(TAG, "onStop() called");
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            Log.d(TAG, "onDestroy() called");
+        }
 }
